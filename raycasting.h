fa640146440028dcf1f0dff4f8e6c9ed16ecd27d
cha202 <116841786+cha202@users.noreply.github.com>
2024-10-01 15:06:19 +0200

    added defines

--- raycasting.h
+++ raycasting.h
@@ -5,14 +5,25 @@
 # include "minilibx/mlx.h"
 # include <X11/X.h>
 # include <X11/keysym.h>
+# include <math.h>
+# include <stdio.h>
 
-int	init_display();
+# define PI 3.14f
+# define CUB 64
+# define FOV (PI / 3)
+# define SCREEN_WIDTH 1050
+# define SCREEN_HEIGHT 550
+# define DISTANCE (SCREEN_WIDTH / 2) / tan(FOV / 2)
+# define ANGLE_BT_RAYS (FOV / (SCREEN_WIDTH - 1))
 
 typedef struct s_data
 {
 	void	*mlx_ptr;
 	void	*win_ptr;
+	char	**map;
 } t_data;
 
+int	init_display(char **map);
+int	raycasting(void);
 
 #endif
