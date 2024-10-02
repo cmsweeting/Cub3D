475788ec14944b675306f987f450f13e6419ab14
cmsweeting <charlottesweeting@outlook.fr>
2024-10-02 15:00:59 +0200

    fix : initialised image after mlx_init and "fixed" initialisation of P_angle

--- display.c
+++ display.c
@@ -19,8 +19,8 @@ void	get_first_ray_angle(t_data *data)
 {
 	int	P_angle;
 
-	if (data->map[data->Py][data->Px] == 'N')
-		P_angle = 0;
+	// if (data->map[data->Py][data->Px] == 'N')
+	P_angle = 0;
 	// else if (data->map[data->Py][data->Px] == 'E')
 	// 	P_angle = 90;
 	// else if (data->map[data->Py][data->Px] == 'S')
@@ -29,7 +29,6 @@ void	get_first_ray_angle(t_data *data)
 	// 	P_angle = 270;
 	data->angle = P_angle - (FOV / 2);
 }
-
 
 int	init_display(char **map)
 {
@@ -42,6 +41,15 @@ int	init_display(char **map)
 	data.mlx_ptr = mlx_init();
 	if (!data.mlx_ptr)
 	 	return (1);
+	data.img.ptr = mlx_new_image(data.mlx_ptr, SCREEN_WIDTH, SCREEN_HEIGHT);
+	if (!data.img.ptr)
+		return (1);
+	data.img.addr = mlx_get_data_addr(data.img.ptr, &data.img.bpp, &data.img.length, &data.img.endian);
+	if (!data.img.addr)
+	{
+		mlx_destroy_image(data.mlx_ptr, data.img.ptr);
+		return (1);
+	}
 	data.win_ptr = mlx_new_window(data.mlx_ptr, SCREEN_WIDTH, SCREEN_HEIGHT, "Cub3D");
 	if (!data.win_ptr)
 	{
@@ -53,6 +61,7 @@ int	init_display(char **map)
 	mlx_hook(data.win_ptr, 17, 0, &close_win, &data);
 	mlx_hook(data.win_ptr, 3, 1L<<1, &escape, &data);
 	mlx_loop(data.mlx_ptr);
+	mlx_destroy_image(data.mlx_ptr, data.img.ptr);
 	mlx_destroy_window(data.mlx_ptr, data.win_ptr);
 	data.win_ptr = NULL;
 	mlx_destroy_display(data.mlx_ptr);
