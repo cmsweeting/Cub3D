42bb32dfe9e57f5fed416cbe1721a88e9d26525c
Coralie Domet De Mont <cdomet-d@z2r2p4.42lyon.fr>
2024-10-03 11:26:03 +0200

    fix: segfault on file extraction

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: cdomet-d <cdomet-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 11:17:49 by cdomet-d          #+#    #+#             */
-/*   Updated: 2024/10/02 11:15:36 by cdomet-d         ###   ########.fr       */
+/*   Updated: 2024/10/03 11:24:01 by cdomet-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,9 @@
 int	main(int ac, char *av[])
 {
 	t_map	map;
+
 	if (ac != 2)
 		return (print_error(EINVAL, "expected one argument"), EINVAL);
 	if (!fetch_map(av[1], &map))
 		return (EINVAL);
-
 }
--- src/parsing/fetch_map.c
+++ src/parsing/fetch_map.c
@@ -6,41 +6,40 @@
 /*   By: cdomet-d <cdomet-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 10:31:22 by cdomet-d          #+#    #+#             */
-/*   Updated: 2024/10/02 16:35:37 by cdomet-d         ###   ########.fr       */
+/*   Updated: 2024/10/03 11:25:44 by cdomet-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
 
-// static void print_darr(char **arr)
-// {
-// 	size_t	i;
-
-// 	i = 0;
-// 	while (arr[i])
-// 	{	
-// 		printf("arr[%ld]: %s", i, arr[i]);
-// 		i++;
-// 	}
-// }
-static bool resize_arr(size_t size, char ***rfile)
+void	print_darr(char **arr)
 {
-	char **copy;
 	size_t	i;
 
 	i = 0;
-	printf("CALL for size: %ld\n", size);
+	while (arr[i])
+	{
+		printf("arr[%ld]: %s", i, arr[i]);
+		i++;
+	}
+}
+
+static bool	resize_arr(size_t size, char ***rfile)
+{
+	char	**copy;
+	size_t	i;
+
+	i = 0;
 	copy = (char **)malloc((size + 1) * sizeof(char *));
 	if (!copy)
 		return (false);
 	copy[size] = NULL;
-	while (*rfile[i])
+	while ((*rfile)[i])
 	{
-		copy[i] = *rfile[i];
+		copy[i] = (*rfile)[i];
 		i++;
 	}
-	// print_darr(copy);
-	free_dtab(*rfile);
+	free(*rfile);
 	*rfile = copy;
 	return (true);
 }
@@ -55,18 +54,17 @@ static bool	extract_file(int _fd, char ***rfile)
 	*rfile = (char **)malloc((size + 1) * sizeof(char *));
 	if (!*rfile)
 		return (false);
-	*rfile[size] = NULL;
-	*rfile[i] = get_next_line(_fd);
+	(*rfile)[size] = NULL;
+	(*rfile)[i] = get_next_line(_fd);
 	if (errno)
 		return (free_dtab(*rfile), false);
-	while (*rfile[i])
+	while ((*rfile)[i])
 	{
 		size++;
 		if (!resize_arr(size, rfile))
 			return (free_dtab(*rfile), false);
-		// *rfile[size] = NULL;
 		i++;
-		*rfile[i] = get_next_line(_fd);
+		(*rfile)[i] = get_next_line(_fd);
 		if (errno)
 			return (free_dtab(*rfile), false);
 	}
@@ -91,14 +89,15 @@ static bool	open_file(char *arg, char ***rfile)
 	if (fd == -1)
 		return (print_error(EINVAL, "could not open file"), false);
 	if (!extract_file(fd, rfile))
-		return (print_error(errno, "during file extraction"), false);
+		return (close(fd), print_error(errno, "during file extraction"), false);
+	close (fd);
 	return (true);
 }
 
 bool	fetch_map(char *arg, t_map *_map)
 {
-	char **rfile;
-	
+	char	**rfile;
+
 	(void)_map;
 	rfile = NULL;
 	if (!arg)
@@ -107,6 +106,7 @@ bool	fetch_map(char *arg, t_map *_map)
 		return (print_error(EINVAL, "extension should be .cub"), false);
 	if (!open_file(arg, &rfile))
 		return (false);
+	print_darr(rfile);
 	free_dtab(rfile);
-	return(true);
+	return (true);
 }
