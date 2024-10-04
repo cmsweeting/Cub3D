6bc759e57bd46194ae731395a19520033dc22b09
cmsweeting <charlottesweeting@outlook.fr>
2024-10-04 18:20:33 +0200

    add : function check_collisions

--- raycasting.c
+++ raycasting.c
@@ -14,20 +14,33 @@
 
 float	get_correct_distance(float horizontal, float vertical, t_data *data, int i)
 {
-	float	distance = 0;
+	float	distance;
 	float	smallest;
-
-	(void)data;
-	float angle_rayon = (i - SCREEN_WIDTH / 2) * 0.0571;
+	float angle;
+	
+	(void)i;
+	distance = 0;
+	angle = 90 - data->ray_angle;//(i - SCREEN_WIDTH / 2) * 0.0571;
 	if (horizontal < vertical || vertical < 0)
 		smallest = horizontal;
 	else
 		smallest = vertical;
-	// if (data->angle < 90)
-	distance = smallest * cosf(angle_rayon * (PI / 180.0f));
-	// else if (data->angle > 90)
-	// 	distance = smallest * cosf(-30 * (PI / 180.0f));
+	distance = smallest * cosf(angle * (PI / 180.0f));
 	return (distance);
+}
+
+int	check_collisions(float x, float y, char **map)
+{
+	int	i;
+	int	j;
+
+	i = y / 64;
+	j = x / 64;
+	if (i < 0 || j < 0 || i > 7 || j > 7 || !map[i][j])
+		return (-1);
+	if (map[i][j] == '1')
+		return (1);
+	return (0);
 }
 
 float	get_vertical_intersection(t_data *data)
@@ -37,29 +50,23 @@ float	get_vertical_intersection(t_data *data)
 	float	Bx;
 	float	By;
 	float	PB;
+	int		i;
 
 	Xa = -64;
-	Ya = -1 * (CUB * tanf(data->angle * (PI / 180.0f)));
-	// printf("Ya : %f\n", Ya);
+	Ya = -1 * (CUB * tanf(data->ray_angle * (PI / 180.0f)));
 	Bx = (int)(data->Px / CUB) * CUB - 1;
-	// printf("Bx : %f\n", Bx);
-	By = (data->Py) - (fabs((data->Px) - Bx) * tanf(data->angle * (PI / 180.0f)));
-	// printf("By : %f\n", By);
-	while (By >= 0 && By != -0 && By != -0 && Bx >= 0 && data->map[(int)(By / CUB)][(int)(Bx / CUB)] \
-		&& data->map[(int)(By / CUB)][(int)(Bx / CUB)] != '1')
+	By = (data->Py) - (fabs((data->Px) - Bx) * tanf(data->ray_angle * (PI / 180.0f)));
+	i = check_collisions(Bx, By, data->map);
+	while (!i)
 	{
 		Bx += Xa;
-		// printf("Bx : %f\n", Bx);
 		By += Ya;
-		// printf("By : %f\n", By);
+		i = check_collisions(Bx, By, data->map);
 	}
-	// printf("Bx coordonnee : %f\n", (Bx / CUB));
-	// printf("By coordonnee : %f\n", (By / CUB));
-	// printf("map[By][Bx] : %c\n", data->map[(int)(By / CUB)][(int)(Bx / CUB)]);
-	if (By < 0 || By == -0 || Bx < 0 || Bx == -0)
+	if (i == -1)
 		PB = -1;
 	else
-		PB = fabs(data->Px * 64 - Bx) / cosf(data->angle * (PI / 180.0f));
+		PB = fabs(data->Px * 64 - Bx) / cosf(data->ray_angle * (PI / 180.0f));
 	return (PB);
 }
 
@@ -70,33 +77,23 @@ float	get_horizontal_intersection(t_data *data)
 	float	Ax;
 	float	Ay;
 	float	PA;
-
-	float angle_radians = data->angle * (PI / 180.0f);
-
-	 if (fabsf(sinf(angle_radians)) < EPSILON)
-        return -1;
+	int		i;
 
 	Ya = -64;
-	Xa = -1 * (64 / tanf(data->angle * (PI / 180.0f)));
-	// printf("Xa : %f\n", Xa);
+	Xa = -1 * (64 / tanf(data->ray_angle * (PI / 180.0f)));
 	Ay = (int)(data->Py / 64) * 64 - 1;
-	// printf("Ay : %f\n", Ay);
-	Ax = data->Px - ((data->Py - Ay) / tanf(data->angle * (PI / 180.0f)));
-	// printf("Ax : %f\n", Ax);
-	while (data->map[(int)(Ay / CUB)][(int)(Ax / CUB)] && data->map[(int)(Ay / CUB)][(int)(Ax / CUB)] != '1')
+	Ax = data->Px - ((data->Py - Ay) / tanf(data->ray_angle * (PI / 180.0f)));
+	i = check_collisions(Ax, Ay, data->map);
+	while (!i)
 	{
 		Ax += Xa;
 		Ay += Ya;
+		i = check_collisions(Ax, Ay, data->map);
 	}
-	// printf("Ax : %f\n", Ax);
-	// printf("Ax coordonnee : %f\n", (Ax / CUB));
-	// printf("Ay : %f\n", Ay);
-	// printf("Ay coordonnee : %f\n", (Ay / CUB));
-	// printf("map[Ay][Ax] : %c\n", data->map[(int)(Ay / CUB)][(int)(Ax / CUB)]);
-	if (data->map[(int)(Ay / CUB)][(int)(Ax / CUB)] != '1')
+	if (i == -1)
 		PA = -1;
 	else
-		PA = fabs(data->Px - Ax) / cosf(data->angle * (PI / 180.0f));
+		PA = fabs(data->Px - Ax) / cosf(data->ray_angle * (PI / 180.0f));
 	return (PA);
 }
 
@@ -108,19 +105,24 @@ int	raycasting(t_data *data)
 	float	distance;
 
 	i = 0;
-	while (i < 33/*SCREEN_WIDTH*/)
+	while (i < 32/*SCREEN_WIDTH*/)
 	{
 		horizontal = get_horizontal_intersection(data);
-		printf("PA : %f\n", horizontal);
-		vertical = -1/*get_vertical_intersection(data)*/;
+		// printf("PA : %f\n", horizontal);
+		vertical = get_vertical_intersection(data);
 		// printf("PB : %f\n", vertical);
 		distance = get_correct_distance(horizontal, vertical, data, i);
-		printf("i : %d : distance : %f\n", i, distance);
+		// printf("i : %d : distance : %f\n", i, distance);
 		draw_column(data, distance, i);
 		i++;
-		data->angle += (i * 0.0571);
-		// printf("data->angle : %f\n", data->angle);
+		data->ray_angle += (i * data->angle_bt_rays);
 	}
 	mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img.ptr, 0, 0);
 	return (0);
 }
+
+
+// float angle_radians = data->ray_angle * (PI / 180.0f);
+
+	//  if (fabsf(sinf(angle_radians)) < EPSILON)
+    //     return -1;
