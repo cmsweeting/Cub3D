f612d8e6eb1e515e5e5f1409b671d67435544278
Charlotte Sweeting <charlottesweeting@outlook.fr>
2024-10-08 17:12:06 +0200

    fix : started fixing operations for movements

--- events.c
+++ events.c
@@ -6,7 +6,7 @@
 /*   By: csweetin <csweetin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 15:05:10 by csweetin          #+#    #+#             */
-/*   Updated: 2024/10/07 18:47:42 by csweetin         ###   ########.fr       */
+/*   Updated: 2024/10/08 16:36:45 by csweetin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,32 +14,34 @@
 
 void	move(int keysym, t_data *data)
 {
+	if (keysym == XK_l)
+		raycasting(data);
 	if (keysym == XK_w)
 	{
-		data->left = true;
-		data->Py -= 10;
-		data->ray_angle = 60;
+		// data->Py -= sin(a) * 0.1
+		// data->Px += cos(a) * 0.1;
+		// OU
+		// data->Px -= cos(a) * 0.1;
 	}
 	else if (keysym == XK_s)
 	{
-		data->left = true;
-		data->Py += 10;
-		data->ray_angle = 60;
+		// data->Py += sin(a) * 0.1;
+		// data->Px += cos(a) * 0.1;
+		// OU
+		// data->Px -= cos(a) * 0.1;
 	}
 	else if (keysym == XK_a)
 	{
-		data->left = true;
-		data->P_angle += (10 * data->angle_bt_rays);
-		data->Px -= 10;
-		data->ray_angle = 60;
+		data->Px -= 0.1;
 	}
 	else if (keysym == XK_d)
 	{
-		data->left = true;
-		data->P_angle += (10 * data->angle_bt_rays);
-		data->Px += 10;
-		data->ray_angle = 60;
+		data->Px += 0.1;
 	}
+	//left arrow :
+	// data->P_angle -= (0.1 * data->angle_bt_rays);
+	//right arrow :
+	// data->P_angle += (0.1 * data->angle_bt_rays);
 }
 
 int	close_win(t_data *data)
